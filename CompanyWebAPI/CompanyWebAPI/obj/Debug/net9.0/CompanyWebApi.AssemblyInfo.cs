f82286860ebf91db9d874e74997b51995af1dee6
HubOl01 <kuloleg54@gmail.com>
2025-07-06 23:04:48 +0300

    update file *.mdf

--- CompanyWebAPI/CompanyWebAPI/obj/Debug/net9.0/CompanyWebApi.AssemblyInfo.cs
+++ CompanyWebAPI/CompanyWebAPI/obj/Debug/net9.0/CompanyWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51409ad9cd8c82ecadd4023aa0fc6990bb2712f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
